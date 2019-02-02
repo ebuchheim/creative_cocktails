da5506208cf15b006d3b5a0f13a64bc4829a0af9
ebuchheim <emma.buchheim@gmail.com>
2019-02-02 10:30:27 -0800

    build out CLI skeleton

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,13 +7,32 @@ class CLI
         puts "3. Surprise me: random inspiration!"
         user_input = gets.chomp
         if user_input == "1"
-            #1 option here
+            drinks_by_name
         elsif user_input == "2"
-            #2 here
+            drinks_by_ingredient
         elsif user_input == "3"
-            #3 here
+            random_drink
         else
             puts "Sorry, I didn't get that! Please choose an option by entering 1, 2, or 3."
         end
+    end
+
+    def drinks_by_name
+        puts "What drink did you have in mind?"
+        user_input = gets.chomp
+        #search the api
+        puts "Here's what I found for #{user_input} recipes:"
+    end
+
+    def drinks_by_ingredient
+        puts "What ingredient did you have in mind?"
+        user_input = gets.chomp
+        #search the api
+        puts "These drinks all include #{user_input}:"
+    end
+
+    def random_drink
+        #search the api
+        puts "Let me tell you about..."
     end
 end
